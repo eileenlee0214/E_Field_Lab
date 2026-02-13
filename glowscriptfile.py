dd5d613829217720a07f72c8aa4e90134583ab12
eileenlee0214 <iamliah1106@gmail.com>
2026-02-13 13:14:49 -0500

    figure out opacity

--- glowscriptfile.py
+++ glowscriptfile.py
@@ -1,5 +1,4 @@
-Web VPython 3.2
-
+# Web VPython 3.2
 def csv_parse(fd): # specify file
     items = []
     text = fd.text
@@ -11,24 +10,34 @@ def csv_parse(fd): # specify file
         
 def grid(values):
     L = len(values)
-    H = len(values[1])
-    for i in range(L):
-        for j in range(H):
+    H = len(values[0])
+    arrow_length = 3
+    
+    for i in range(L-1):
+        for j in range(H-1):
             volt = values[i][j]
-            dvx = values[i+1][j] - values[i][j]
-            dvy = values[i][j+1] - values[i][j]
-            arrow(pos = vector(j*4-(2*H), i*4-(2*L), 0), axis=vec(3,1,0), color=color.orange)
+
+            dvx = float(values[i][j+1]) - float(values[i][j])
+            dvy = float(values[i+1][j]) - float(values[i][j])
             
-            # x positions increase to the right, but y positions 
-            # increase going upwards
+            Ex = -dvx
+            Ey = -dvy
+            
+            E_norm = sqrt(Ex**2 + Ey**2)
+            
+            if E_norm > 0:
+                Ex_normalized = (Ex / E_norm) * arrow_length
+                Ey_normalized = (Ey / E_norm) * arrow_length
+            else:
+                Ex_normalized = 0
+                Ey_normalized = 0
+            
+            arrow(pos = vector(j*4-(2*H), i*4-(2*L), 0), 
+                  axis = vector(Ex_normalized, Ey_normalized, 0), 
+                  color = color.orange)
                 
     
 f = read_local_file(scene.title_anchor)
 floatages = csv_parse(f)
 voltages = [[float(item) for item in sublist] for sublist in floatages]
-
-
-
 grid(voltages)
-
- 
