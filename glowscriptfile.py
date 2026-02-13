ecc99b5b97945943279f05b6ab50cd29cda97a22
eileenlee0214 <iamliah1106@gmail.com>
2026-02-12 22:20:17 -0500

    glowscript file added

---
+++ glowscriptfile.py
@@ -0,0 +1,29 @@
+Web VPython 3.2
+
+def csv_parse(fd): # specify file
+    items = []
+    text = fd.text
+    for line in iter(text.splitlines()):
+        if len(line.strip()) > 1 :
+            items.append(line.strip().split(','))
+    return items
+            
+        
+def grid(values):
+    L = len(values)
+    H = len(values[1])
+    for i in range(L):
+        for j in range(H):
+            volt = values[i][j]
+            text(pos = vector(j*4-(2*H), i*4-(2*L), 0), text=volt)
+            # x positions increase to the right, but y positions 
+            # increase going upwards
+                
+    
+f = read_local_file(scene.title_anchor)
+floatages = csv_parse(f)
+voltages = [[float(item) for item in sublist] for sublist in floatages]
+
+grid(voltages)
+
+ 
